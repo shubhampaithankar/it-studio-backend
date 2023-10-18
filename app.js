75d0f914ce3697506de14281ec97abb5e7ebc8b1
shubhampaithankar <shubham.paithankar@krayo.io>
2023-10-18 13:49:20 +0530

    added cors

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // app.js
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const connectDatabase = require('./src/config/database')
 const routes = require('./src/routes/index')
 
 require('dotenv').config()
 
+
 const app = express()
 const PORT = process.env.PORT
 
 connectDatabase()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/', routes)
 
